6fb045cc844774db9620e2d703eb7557814a9abe
SoulDevAdmin <sysadmin@soulsociety.gg>
2023-09-11 14:51:10 +0900

    Add HonContract

---
+++ hon/contracts/HonContract.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract HONToken is ERC20, Ownable {
+
+    constructor(uint256 initialSupply) ERC20("HON", "HON") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public onlyOwner {
+        _burn(from, amount);
+    }
+}
