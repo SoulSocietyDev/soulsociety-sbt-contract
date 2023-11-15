2455f4446f34d63d5bbeb06141d8f8ecdda048e5
Jayce <>
2023-11-15 17:34:47 +0900

    Add HonContract for foundry

---
+++ hon-contract/src/HonContract.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract HONToken is ERC20, Ownable {
+
+    uint256 private constant MAX_SUPPLY = 1000000000 * (10 ** 18);
+
+    constructor() ERC20("HON Token", "HON") {
+        _mint(msg.sender, MAX_SUPPLY);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        require(totalSupply() + amount <= MAX_SUPPLY, "Minting would exceed max supply");
+
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public onlyOwner {
+        _burn(from, amount);
+    }
+}
